491ce120470820ddb18036a634ce1723e0bbe4dd
Aspharier <codeworkashish@gmail.com>
2024-07-20 22:22:53 +0530

    third commit

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -7,11 +11,33 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(
-        children: [
-          Image.asset('lib/images/nikenew.png',
-          height: 240,),
-        ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset('lib/images/nikenew.png',
+                height: 240,),
+              ),
+              const SizedBox(
+                height: 48,
+              ),
+              const Text('Just Do It',
+              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+
+              const SizedBox(height: 24,),
+
+              const Text('Brand new sneakers and custom kicks made with premium quality',
+              style: TextStyle(fontSize: 16,color: Colors.grey,),textAlign: TextAlign.center,),
+
+              const SizedBox(height: 24,),
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
