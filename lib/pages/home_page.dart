91419bc8c634d6767844e8cbd8f6afce17a574dc
Aspharier <codeworkashish@gmail.com>
2024-07-22 22:33:55 +0530

    fivth commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sneakershopping_flutter/components/bottom_nav_bar.dart';
+import 'package:sneakershopping_flutter/pages/cart_page.dart';
+import 'package:sneakershopping_flutter/pages/shop_page.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -7,8 +10,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  int _selectedIndex = 0;
+  void navigateBottomBar(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+
+    const CartPage(),
+
+
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      backgroundColor: Colors.grey[300],
+      body: _pages[_selectedIndex],
+    );
   }
 }
